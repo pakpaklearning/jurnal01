26cf4512b9e519cb89b3c5c3cb52222095221d64
pakpaklearning <zidaneahmad456@gmail.com>
2023-09-27 09:36:53 +0700

    commit pertama

---
+++ Jurnal01.java
@@ -0,0 +1,9 @@
+public class Jurnal01{
+ 
+    public static void main (String [] args) {
+
+         System.out.println("NAMA : ZIDANE AHMAD FIRDAUS ");
+         System.out.println("NIM : 607062300124");
+         System.out.println("KELAS : D3IF-47-04 ");
+    }
+}
